cd24b56e014b1be7a2d480a2cffe259e0c01442a
Manuel Gosende <mgosende5@gmail.com>
2023-08-01 01:06:52 -0300

    Estilado de Modal finalizado.

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useModal } from "../hooks/useModal";
+import { useCart } from "../hooks/useCart";
 
 type ModalComponentProps = {
   setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,16 +9,35 @@ type ModalComponentProps = {
 
 export const ModalComponent: React.FC<ModalComponentProps> = ({ setShowCart, closeModal }) => {
 
+  const { gems } = useCart();
+
   const handleContinue = () => {
       closeModal();
       setShowCart(true);
   }
 
   return (
-    <div className='modal is-open'>
-      <div className='modal-container'>
-        <button onClick={closeModal}>Volver al marketplace</button>
-        <button onClick={handleContinue}>Continuar al carrito</button>
+    <div className='modal'>
+      <div className='flex flex-col space-y-6 font-sans bg-stone-700 p-6 rounded-lg modal-container'>
+        <h1 className='font-bold text-3xl'>Todavía podés utilizar { gems } { gems == 1 ? "Gema" : "Gemas" }</h1>
+        <p className='text-stone-400 text-sm'>Para poder vencer al 🐉 es mejor contar con todas las pociones posibles.</p>
+        <div className='flex flex-col space-y-4 text-center'>
+          <h3>¿Qué deseas hacer?</h3>
+          <div className='flex flex-row space-x-4 justify-around items-center'>
+            <button
+              className='w-full text-center bg-stone-300 p-2 rounded-full text-black text-sm'
+              onClick={closeModal}
+            >
+              Volver al marketplace
+            </button>
+            <button
+            className='w-full text-center bg-blue-700 p-2 rounded-full text-white text-sm'
+              onClick={handleContinue}
+            >
+              Continuar al carrito
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
